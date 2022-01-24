5acbf708c36bb2f412f7eb6f53f7be0d6455e466
Michael Wroblewski <michael.wroblewski08@yahoo.com.au>
2022-01-24 21:46:59 +1100

    Add Week 1 scripts to portfolio

---
+++ scripts/portfolio/week1/first.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Introduces the 'echo' command
+# Michael - 2022/01/24
+
+echo "Hi there!"
+echo "Word"
+exit 0
---
+++ scripts/portfolio/week1/second.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Introduces the use of arguments.
+# Michael - 2022/01/24
+
+echo "Hi there!"
+echo "It's good to see you $1!"
+exit 0
