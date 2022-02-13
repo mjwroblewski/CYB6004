777d44f688a9fb04d6fc86655cc385b2c63e78b7
Michael Wroblewski <michael.wroblewski08@yahoo.com.au>
2022-02-13 15:25:36 +1100

    Update downloader.sh, now downloads a single URL

--- scripts/portfolio/week3/downloader.sh
+++ scripts/portfolio/week3/downloader.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 
-echo "Welcome to the website downloader."
-read -p "Please type the website URL to download, or enter 'q' to quit: " URL
+# Download a webpage and save to selected location
+# Michael 2022/02/08
 
+echo "Welcome to the webpage downloader."
 
+read -p "Please type the webpage URL to download, or enter 'q' to quit: " URL
 
 if [ "$URL" = "q" ]; then
     echo "Goodbye!"
-    
+
+    # Script terminates if user selects 'q'
+
     exit 0
 
-break
-
 else
-    read -p "Please type the website download location: " DLOC
-    
+
+    read -p "Please type the directory to save the webpage: " DLOC
+        
+        wget -p $DLOC $URL
+
+    echo "Website has been downloaded."
+
 fi
 
-wget --mirror --convert-links --page-requisites --no-parent -P $DLOC $URL
-
-#E:/Education/Cyber_Security/ECU/CYB6004-Scripting_languages/home/student/scripts/ 
-
-# documents/websites/ $URL
-
-# wget $URL -o -r -p 
-
-echo "Website has been downloaded."
-
-exit -0
+exit 0
 
 
 # References
-#
-#
-# https://linuxconfig.org/download-file-from-url-on-linux-using-command-line
-# https://alvinalexander.com/linux-unix/wget-command-shell-script-example-download-url/
-# https://www.hostinger.com/tutorials/wget-command-examples/
+    # CYB6004.2022.AC1.OFFCAMPUS Scripting Languages
+        # Week 3 - Snippets and repetition 
+        # 3.5 An internet downloader using loops
+    # https://linuxconfig.org/download-file-from-url-on-linux-using-command-line
+    # https://alvinalexander.com/linux-unix/wget-command-shell-script-example-download-url/
+    # https://www.hostinger.com/tutorials/wget-command-examples/
+    # https://likegeeks.com/bash-scripting-step-step-part2/
