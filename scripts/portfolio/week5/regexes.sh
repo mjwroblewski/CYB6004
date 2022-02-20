34c43bec4cefe7245d477ed7abe615f37e493e56
Michael Wroblewski <michael.wroblewski08@yahoo.com.au>
2022-02-21 00:11:22 +1100

    Add week 5 script regexes.sh

---
+++ scripts/portfolio/week5/regexes.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+#Show only lines that start with 'echo'.     
+    grep -r "^echo" 
+
+ #Show only statements that contain an echo with a flag.    
+    grep -r 'echo -.*' 
+     
+#Show only echo statements where the text is in double quotes and starts with an uppercase letter.
+    grep -r 'echo .*\"[A-Z].*\"' 
+     
+
+#Show only echo statements with text in double quotes that ends in an exclamation mark.
+    grep -r 'echo .*\".*!\"' 
+     
+#all sed statements
+    grep -r "sed"
+
+#all lines that start with the letter m
+    grep -r "^m"
+
+#all lines that contain three digit numbers
+    grep -r '[0-9]{3}'
+
+#all echo statements with at least three words
+    grep -r 'echo (\w+\W+){3,}'
+
+#all lines that would make a good password (use your knowledge of cybersecurity to decide what makes a good password).
