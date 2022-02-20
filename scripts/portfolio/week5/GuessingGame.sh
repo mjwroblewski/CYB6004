530497ec610969de73007b24b2fac93c000e0b6d
Michael Wroblewski <michael.wroblewski08@yahoo.com.au>
2022-02-21 00:19:43 +1100

    Add updated script GuessingGame.sh to portfolio

---
+++ scripts/portfolio/week5/GuessingGame.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# The script loops and displays an error message until the user enters the correct value. Updated in Week 5
+# Michael Wroblewski - 2022/02/21
+
+printError()
+
+{
+    echo -e "\033[31mERROR:\033[0m $1" 
+}
+
+GuessNumber()
+
+{
+    read -p "$1: "
+    
+    while (( $REPLY < $2 )); do
+
+    printError "Too low, guess again!"
+
+        read -p "$1: "
+
+        while (( $REPLY > $2 )); do
+
+        printError "Too high, guess again!"
+
+            read -p "$1: "
+
+            while (( $REPLY == $2 )); do
+
+            echo "Right!"
+
+            break
+
+            done
+            
+        done
+        
+    done
+    }
+
+GuessNumber "Please guess a number between 1 and 100" 42
+
+# References
+   # CYB6004.2022.AC1.OFFCAMPUS Scripting Languages
+        # Week 4 - Functions and editing code
+        # 4.2 Understand and execute scripts that use multiple functions
+        # 4.3 Putting it together
+ 
