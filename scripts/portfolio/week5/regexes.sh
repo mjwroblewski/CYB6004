07e29768adac7324ee9ebe5c138a73caff9ef24f
Michael Wroblewski <michael.wroblewski08@yahoo.com.au>
2022-02-21 00:24:51 +1100

    Comments updated for regexes.sh

--- scripts/portfolio/week5/regexes.sh
+++ scripts/portfolio/week5/regexes.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# A variety of regex commands
+# Michael Wroblewski - 2022/02/21
 
 #Show only lines that start with 'echo'.     
     grep -r "^echo" 
@@ -27,3 +29,11 @@
     grep -r 'echo (\w+\W+){3,}'
 
 #all lines that would make a good password (use your knowledge of cybersecurity to decide what makes a good password).
+
+    
+
+# References
+   # CYB6004.2022.AC1.OFFCAMPUS Scripting Languages
+        # Week 5 - Regular expressions and text manipulation
+        # 5.2 Develop your own regular expressions to improve text parsing scripts
+    # https://www.youtube.com/watch?v=sa-TUpSx1JA
