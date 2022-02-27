c441b1969d4c6207c4e493d01112b72d1f51d689
mjwroblewski <97830243+mjwroblewski@users.noreply.github.com>
2022-02-27 15:24:43 +1100

    Updated script comments

--- scripts/portfolio/week1/first.sh
+++ scripts/portfolio/week1/first.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-# Introduces the 'echo' command
-# Michael - 2022/01/24
+# Created by Michael Wroblewski
+# Date created - 2022/01/24
+# Date last modified - 2022/02/27
+
+# This script introduces use of the 'echo' command
+# The text within quotes " " is displayed when the script is executed
+
 
 echo "Hi there!"
 echo "Word"
 exit 0
+
+# References
+#   CYB6004 - Scripting languages
+#       Week 1: Introduction to Scripting and the Linux environment in a cybersecurity context
+#       1.4 Write and execute simple shell scripts
