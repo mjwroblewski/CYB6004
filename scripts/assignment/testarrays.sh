9b9a8c2c3d1595c3b5077d04370e7daea3e65443
mjwroblewski <97830243+mjwroblewski@users.noreply.github.com>
2022-03-05 00:53:18 +1100

    Test file

---
+++ scripts/assignment/testarrays.sh
@@ -0,0 +1,414 @@
+#!/bin/bash
+
+
+    hack1="1:2011:Mt Gox:Bitcoin:Undisclosed:8,750,000:0:Stolen credentials"                         # 
+    hack2="2:2011:Bitcoin7:-:-:-:-:Data - Undisclosed"                                               # 
+    hack3="3:2012:Bitcoinica:Bitcoin:43,554:228,000:0:Hot wallet"                                    # 
+    hack4="4:2012:Bitcoinica:Bitcoin:38,000:87,000:0:Hot wallet"                                     # 
+    hack5="5:2012:Bitcoinica:Bitcoin:40,000:300,000:300,000:Undisclosed"                             # 
+    hack6="6:2012:Bitfloor:Bitcoin:24,000:250,000:0:Undisclosed"                                     # 
+    hack7="7:2013:Vircurex:Various:250,117:352,000:0:Stolen credentials"                             # 
+    hack8="8:2013:Picostocks:Bitcoin:1,300:Undisclosed:0:Undisclosed"                                # 
+    hack9="9:2013:Inputs.io:Bitcoin:4,100:1,200,000:0:Server vulnerability"                          # 
+    hack10="10:2014:Mt Gox:Bitcoin:844,408:661,348,000:0:Undisclosed"                                # 
+    hack11="11:2014:Picostocks:Bitcoin:5,896:4,434,000:0:Cold wallet"                                # 
+    hack12="12:2014:Flexcoin:Bitcoin:896:600,000:600,000:Hot wallet"                                 # 
+    hack13="13:2014:Poloniex:Bitcoin:97:67,500:67,500:Undisclosed"                                   #
+    hack14="14:2014:Crypto Rush:Various:3,450:630,000:630,000:Undisclosed"                           #
+    hack15="15:2014:MintPal:Vericoin:8,000,000:1,933,000:0:Hot wallet"                               #
+    hack16="16:2014:Cryptysy:Various:313,000:9,580,000:0:Undisclosed"                                #
+    hack17="17:2015:Bitstamp:Bitcoin:19,000:5,100,000:0:Cold wallet"                                 # 
+    hack18="18:2015:796:Bitcoin:1,000:270,000:0:Internal error"                                      #
+    hack19="19:2015:BTER:Bitcoin:7,170:1,750,000:0:Cold wallet"                                      #
+    hack20="20:2015:KipCoin:Bitcoin:3,000:728,000:0:Hot wallet"                                      #
+    hack21="21:2015:Bitfinex:Bitcoin:1,400:329,000:0:Hot wallet"                                     #
+    hack22="22:2016:Gatecoin:Various:185,250:2,140,000:0:Undisclosed"                                #
+    hack23="23:2016:ShapeShift:Various:Undisclosed:200,000:0:Hot wallet"                             #
+    hack24="24:2016:Bitfinex:Bitcoin:120,000:72,000,000:0:Undisclosed"                               #
+    hack25="25:2016:Bitcurex:Bitcoin:Undisclosed:1,500,000:0:Undisclosed"                            #
+    hack26="26:2017:Yapizon:Bitcoin:3,831:4,850,000:0:Undisclosed"                                   #
+    hack27="27:2017:Bithumb:-:-:-:-:Data - Undisclosed"                                              #
+    hack28="28:2017:YouBit:Undisclosed:Undisclosed:Undisclosed:Undisclosed:Undisclosed"              #
+    hack29="29:2018:LocalBitcoins:Bitcoin:7.9:27,000:0:Undisclosed"                                  # 
+    hack30="30:2018:Coincheck:NEM:500,000,000:500,000,000:0:Undisclosed"                             #
+    hack31="31:2018:BitGrail:Nano:17,000,000:170,000,000:0:Undisclosed"                              #
+    hack32="32:2018:Coinrail:Various:Undisclosed:40,000,000:0:Undisclosed"                           #
+    hack33="33:2018:Bithumb:Various:Undisclosed:31,000,000:0:Undisclosed"                            #
+    hack34="34:2018:Bancor:Undisclosed:Undisclosed:23,000,000:0:Undisclosed"                         #
+    hack35="35:2018:Zaif:Various:Undisclosed:60,000,000:0:Undisclosed"                               #
+    hack36="36:2018:MapleChange:Undisclosed:Undisclosed:Undisclosed:Undisclosed:Undisclosed"         #
+    hack37="37:2019:Cryptopia:Various:48,019,391:3,620,000:0:Undisclosed"                            # 
+    hack38="38:2019:Coinmama:-:-:-:-:Data - Undisclosed"                                             #
+    hack39="39:2019:DragonEX:Undisclosed:Undisclosed:Undisclosed:Undisclosed:Undisclosed"            #
+    hack40="40:2019:Coinbene:Undisclosed:Undisclosed:100,000,000:0:Undisclosed"                      #
+    hack41="41:2019:Bithumb:EOS:Undisclosed:13,000,000:0:Internal theft"                             #
+    hack42="42:2019:Bitrue:Undisclosed:Undisclosed:4,200,000:0:Undisclosed"                          #
+    hack43="43:2019:BITPoint:Cash:32,000,000:32,000,000:0:Hot wallet"                                #
+    hack44="44:2019:Upbit:Ethereum:342,000:49,000,000:0:Hot wallet"                                  #
+    hack45="45:2020:Altsbit:Various:Undisclosed:73,000:0:Undisclosed"                                # 
+    hack46="46:2020:BlockFi:-:-:-:-:Unsuccessful"                                                    #
+    hack47="47:2020:BuyUcoin:-:-:-:-:Data - Undisclosed"                                             #
+    hack48="48:2020:Eterbase:Undisclosed:Undisclosed:5,400,000:0:Undisclosed"                        #
+    hack49="49:2020:KuCoin:Undisclosed:Undisclosed:280,000:0:Hot wallet key theft"                   #
+    hack50="50:2020:Livecoin:-:-:-:-:Undisclosed"                                                    #
+    hack51="51:2021:Cryptopia:Undisclosed:Undisclosed:45,000:45,000:Undisclosed"                     # 
+    hack52="52:2021:Hotbit:-:-:-:-:Unsuccessful"                                                     #
+    hack53="53:2021:Liquid:Undisclosed:Undisclosed:80,000,000:0:Hot wallet"                          #
+    hack54="54:2021:BitMart:Undisclosed:Undisclosed:150,000,000:0:Undisclosed"                       #
+    hack55="55:2021:AscendEX:Undisclosed:Undisclosed:78,000,000:0:Undisclosed"                       #
+    hack56="56:2022:LCX:Various:Undisclosed:6,800,000:0:Hot wallet"                                  # 
+                                                                                                     #
+######################################################################################################
+
+# "arrays" function inserts a delimiter "#" at the beginning of variables "$hack1 - $hack56 then copies variables to $arraysfile
+# "sed" inserts line breaks at delimiters, deletes the blank top line then deletes the delimeter
+# The "$arraysfile" document is the source for user-generated search results
+
+arraysfile=arrays.txt
+
+black="\033[30m"
+red="\033[31m"
+green="\033[32m"
+brown="\033[33m"
+blue="\033[34m"
+purple="\033[35m"
+cyan="\033[36m"
+clear="\033[0m"
+
+    U_sum="awk '{ S+=\$5} END {print Total units stolen: S}'"
+    V_sum="awk '{ S+=\$6} END {print Total value stolen: \$S}'"
+    R_sum="awk '{ S+=\$7} END {print Total value recovered: \$S}'"
+
+#Year variables
+
+Y_2011_file="Y_2011_file.txt"
+Y_2012_file="Y_2012_file.txt"
+Y_2013_file="Y_2013_file.txt"
+Y_2014_file="Y_2014_file.txt"
+Y_2015_file="Y_2015_file.txt"
+Y_2016_file="Y_2016_file.txt"
+Y_2017_file="Y_2017_file.txt"
+Y_2018_file="Y_2018_file.txt"
+Y_2019_file="Y_2019_file.txt"
+Y_2020_file="Y_2020_file.txt"
+Y_2021_file="Y_2021_file.txt"
+Y_2022_file="Y_2022_file.txt"
+
+#Year variables
+
+Y_2011_T="Y_2011_T.txt"
+Y_2012_T="Y_2012_T.txt"
+Y_2013_T="Y_2013_T.txt"
+Y_2014_T="Y_2014_T.txt"
+Y_2015_T="Y_2015_T.txt"
+Y_2016_T="Y_2016_T.txt"
+Y_2017_T="Y_2017_T.txt"
+Y_2018_T="Y_2018_T.txt"
+Y_2019_T="Y_2019_T.txt"
+Y_2020_T="Y_2020_T.txt"
+Y_2021_T="Y_2021_T.txt"
+Y_2022_T="Y_2022_T.txt"
+
+# Hack variables
+
+H_2011=hack{1..2}
+H_2012=hack{3..6}
+H_2013=hack{7..9}
+H_2014=hack{10..16}
+H_2015=hack{17..21}
+H_2016=hack{22..25}
+H_2017=hack{26..28}
+H_2018=hack{29..36}
+H_2019=hack{37..44}
+H_2020=hack{45..50}
+H_2021=hack{51..55}
+H_2022=hack56
+
+
+
+    function arrays() {
+
+        echo -e \#$hack{1..56} > arrays.txt
+
+            sed -i 's/#/\n&/g' $arraysfile
+
+                sed -i '1d' $arraysfile
+    
+                    sed -i 's/#//g' $arraysfile
+
+                cat $arraysfile > arrays_T.txt 
+
+                    sed -i 's/,//g' arrays_T.txt
+    }
+
+    function Y_2011() {
+
+        echo -e \#$hack{1..2} > Y_2011_file.txt
+
+            sed -i 's/#/\n&/g' $Y_2011_file
+
+                sed -i '1d' $Y_2011_file
+    
+                    sed -i 's/#//g' $Y_2011_file
+
+        cat $Y_2011_file > $Y_2011_T
+        
+            sed -i 's/,//g' $Y_2011_T
+
+        wc -l $Y_2011_file | awk '{print "Total hacks conducted: "$1}'
+        awk '{ S+=$5 } END {print "Total units stolen: "S}' $Y_2011_T
+        awk '{ S+=$6 } END {print "Total value stolen: $"S}' $Y_2011_T 
+        awk '{ S+=$7 } END {print "Total value recovered: $"S}' $Y_2011_T 
+      }
+
+
+    function Y_2012() {
+
+        echo -e \#$hack{3..6} > Y_2012_file.txt
+
+            sed -i 's/#/\n&/g' $Y_2012_file
+
+                sed -i '1d' $Y_2012_file
+    
+                    sed -i 's/#//g' $Y_2012_file
+
+        cat $Y_2012_file > $Y_2012_T
+        
+            sed -i 's/,//g' $Y_2012_T
+
+        wc -l $Y_2012_file | awk '{print "Total hacks conducted: "$1}'
+        awk '{ S+=$5 } END {print "Total units stolen: "S}' $Y_2012_T
+        awk '{ S+=$6 } END {print "Total value stolen: $"S}' $Y_2012_T 
+        awk '{ S+=$7 } END {print "Total value recovered: $"S}' $Y_2012_T 
+      }
+
+    function Y_2013() {
+      
+        echo -e \#$hack{7..9} > Y_2013_file.txt
+
+            sed -i 's/#/\n&/g' $Y_2013_file
+
+                sed -i '1d' $Y_2013_file
+    
+                    sed -i 's/#//g' $Y_2013_file
+
+        cat $Y_2013_file > $Y_2013_T
+        
+            sed -i 's/,//g' $Y_2013_T          
+
+        wc -l $Y_2013_file | awk '{print "Total hacks conducted: "$1}'
+        awk '{ S+=$5 } END {print "Total units stolen: "S}' $Y_2013_T
+        awk '{ S+=$6 } END {print "Total value stolen: $"S}' $Y_2013_T 
+        awk '{ S+=$7 } END {print "Total value recovered: $"S}' $Y_2013_T
+      }
+    
+    function Y_2014() {
+      
+        echo -e \#$hack{10..16} > Y_2014_file.txt
+
+            sed -i 's/#/\n&/g' $Y_2014_file
+
+                sed -i '1d' $Y_2014_file
+    
+                    sed -i 's/#//g' $Y_2014_file
+
+        cat $Y_2014_file > $Y_2014_T
+        
+            sed -i 's/,//g' $Y_2014_T          
+      
+        wc -l $Y_2014_file | awk '{print "Total hacks conducted: "$1}'
+        awk '{ S+=$5 } END {print "Total units stolen: "S}' $Y_2014_T
+        awk '{ S+=$6 } END {print "Total value stolen: $"S}' $Y_2014_T 
+        awk '{ S+=$7 } END {print "Total value recovered: $"S}' $Y_2014_T
+      }
+        
+    function Y_2015() {
+      
+        echo -e \#$hack{17..21} > Y_2015_file.txt
+
+            sed -i 's/#/\n&/g' $Y_2015_file
+
+                sed -i '1d' $Y_2015_file
+    
+                    sed -i 's/#//g' $Y_2015_file
+
+        cat $Y_2015_file > $Y_2015_T
+        
+            sed -i 's/,//g' $Y_2015_T          
+      
+        wc -l $Y_2015_file | awk '{print "Total hacks conducted: "$1}'
+        awk '{ S+=$5 } END {print "Total units stolen: "S}' $Y_2015_T
+        awk '{ S+=$6 } END {print "Total value stolen: $"S}' $Y_2015_T 
+        awk '{ S+=$7 } END {print "Total value recovered: $"S}' $Y_2015_T
+      }
+
+    function Y_2016() {
+      
+        echo -e \#$hack{22..25} > Y_2016_file.txt
+
+            sed -i 's/#/\n&/g' $Y_2016_file
+
+                sed -i '1d' $Y_2016_file
+    
+                    sed -i 's/#//g' $Y_2016_file
+
+        cat $Y_2016_file > $Y_2016_T
+        
+            sed -i 's/,//g' $Y_2016_T          
+      
+        wc -l $Y_2016_file | awk '{print "Total hacks conducted: "$1}'
+        awk '{ S+=$5 } END {print "Total units stolen: "S}' $Y_2016_T
+        awk '{ S+=$6 } END {print "Total value stolen: $"S}' $Y_2016_T 
+        awk '{ S+=$7 } END {print "Total value recovered: $"S}' $Y_2016_T
+      }        
+
+    function Y_2017() {
+      
+        echo -e \#$hack{26..28} > Y_2017_file.txt
+
+            sed -i 's/#/\n&/g' $Y_2017_file
+
+                sed -i '1d' $Y_2017_file
+    
+                    sed -i 's/#//g' $Y_2017_file
+
+        cat $Y_2017_file > $Y_2017_T
+        
+            sed -i 's/,//g' $Y_2017_T          
+
+        wc -l $Y_2017_file | awk '{print "Total hacks conducted: "$1}'
+        awk '{ S+=$5 } END {print "Total units stolen: "S}' $Y_2017_T
+        awk '{ S+=$6 } END {print "Total value stolen: $"S}' $Y_2017_T 
+        awk '{ S+=$7 } END {print "Total value recovered: $"S}' $Y_2017_T
+      }
+    
+    function Y_2018() {
+      
+        echo -e \#$hack{29..36} > Y_2018_file.txt
+
+            sed -i 's/#/\n&/g' $Y_2018_file
+
+                sed -i '1d' $Y_2018_file
+    
+                    sed -i 's/#//g' $Y_2018_file
+
+        cat $Y_2018_file > $Y_2018_T
+        
+            sed -i 's/,//g' $Y_2018_T          
+
+        wc -l $Y_2018_file | awk '{print "Total hacks conducted: "$1}'
+        awk '{ S+=$5 } END {print "Total units stolen: "S}' $Y_2018_T
+        awk '{ S+=$6 } END {print "Total value stolen: $"S}' $Y_2018_T 
+        awk '{ S+=$7 } END {print "Total value recovered: $"S}' $Y_2018_T
+      }
+    
+    function Y_2019() {
+      
+        echo -e \#$hack{37..44} > Y_2019_file.txt
+
+            sed -i 's/#/\n&/g' $Y_2019_file
+
+                sed -i '1d' $Y_2019_file
+    
+                    sed -i 's/#//g' $Y_2019_file
+
+        cat $Y_2019_file > $Y_2019_T
+        
+            sed -i 's/,//g' $Y_2019_T          
+
+        wc -l $Y_2019_file | awk '{print "Total hacks conducted: "$1}'
+        awk '{ S+=$5 } END {print "Total units stolen: "S}' $Y_2019_T
+        awk '{ S+=$6 } END {print "Total value stolen: $"S}' $Y_2019_T 
+        awk '{ S+=$7 } END {print "Total value recovered: $"S}' $Y_2019_T
+      }
+    
+    function Y_2020() {
+      
+        echo -e \#$hack{45..50} > Y_2020_file.txt
+
+            sed -i 's/#/\n&/g' $Y_2020_file
+
+                sed -i '1d' $Y_2020_file
+    
+                    sed -i 's/#//g' $Y_2020_file
+
+        cat $Y_2020_file > $Y_2020_T
+        
+            sed -i 's/,//g' $Y_2020_T          
+
+        wc -l $Y_2020_file | awk '{print "Total hacks conducted: "$1}'
+        awk '{ S+=$5 } END {print "Total units stolen: "S}' $Y_2020_T
+        awk '{ S+=$6 } END {print "Total value stolen: $"S}' $Y_2020_T 
+        awk '{ S+=$7 } END {print "Total value recovered: $"S}' $Y_2020_T
+      }
+    
+    function Y_2021() {
+      
+        echo -e \#$hack{51..55} > Y_2021_file.txt
+
+            sed -i 's/#/\n&/g' $Y_2021_file
+
+                sed -i '1d' $Y_2021_file
+    
+                    sed -i 's/#//g' $Y_2021_file
+
+        cat $Y_2021_file > $Y_2021_T
+        
+            sed -i 's/,//g' $Y_2021_T          
+
+        wc -l $Y_2021_file | awk '{print "Total hacks conducted: "$1}'
+        awk '{ S+=$5 } END {print "Total units stolen: "S}' $Y_2021_T
+        awk '{ S+=$6 } END {print "Total value stolen: $"S}' $Y_2021_T 
+        awk '{ S+=$7 } END {print "Total value recovered: $"S}' $Y_2021_T
+      }
+    
+    function Y_2022() {
+      
+        echo -e \#$hack56 > Y_2022_file.txt
+
+            sed -i 's/#/\n&/g' $Y_2022_file
+
+                sed -i '1d' $Y_2022_file
+    
+                    sed -i 's/#//g' $Y_2022_file
+
+        cat $Y_2022_file > $Y_2022_T
+        
+            sed -i 's/,//g' $Y_2022_T          
+
+        wc -l $Y_2022_file | awk '{print "Total hacks conducted: "$1}'
+        awk '{ S+=$5 } END {print "Total units stolen: "S}' $Y_2022_T
+        awk '{ S+=$6 } END {print "Total value stolen: $"S}' $Y_2022_T 
+        awk '{ S+=$7 } END {print "Total value recovered: $"S}' $Y_2022_T
+      }
+        
+    function table_all_data() {
+                      
+        awk -F":" '
+
+        BEGIN {
+    
+        printf ("_____________________________________________________________________________________________________________________\n");
+
+        printf ("%-5s %-5s %-15s %-15s %-14s %-14s %-8s %-0s \n","| Hack","| Year","| Exchange","| Currency","| Units"," | \$ Value"," | \$ Recovered","| Vulnerability          |");
+ 
+        printf ("_____________________________________________________________________________________________________________________\n");
+
+    }
+
+    {
+        printf ("| %-4s | %-4s | %-13s | %-13s | %-13s | %-12s | %-11s | %-22s |\n", $1, $2, $3, $4, $5, $6, $7, $8 );
+
+        printf ("_____________________________________________________________________________________________________________________\n")
+
+
+    }' $Y_2018_file
+
+    }
+    
+    arrays
+    table_all_data
+    Y_2018
+
+
+
